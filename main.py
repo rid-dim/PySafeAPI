3565ffe86520f915642130e700d2a92cd27cc454
hintofbasil <hintofbasil@github.com>
2016-06-17 15:19:23 +0100

    Added GET /auth

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ class Safe:
         self.id = id
         self.url = "%s:%d/" % (addr, port)
 
+    def get_url(self, location):
+        return self.url + location
+
     def authenticate(self, permissions=[]): #TODO check is needs to = None
         keys = PrivateKey.generate()
         nonce = nacl.utils.random(Box.NONCE_SIZE)
@@ -52,6 +55,25 @@ class Safe:
             return True
         else:
             return False
+
+    def is_authenticated(self):
+        try: # If not token saved definitely not authenticated
+            self.token
+        except AttributeError:
+            return False
+
+        headers = {
+                'Authorization': 'Bearer %s' % self.token
+        }
+        r = response = requests.get(
+                self.get_url('auth'),
+                headers=headers
+        )
+        if r.status_code == 200:
+            return True
+        else:
+            return False
+
 
 if __name__=='__main__':
     s = Safe('Test', '0.0.1', 'hintofbasil', 'com.github.hintofbasil')
