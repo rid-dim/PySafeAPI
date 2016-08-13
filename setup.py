0457867bffe1fb6b30108284be0a07516d4723cb
William Hutcheson <hintofbasil@github.com>
2016-08-13 17:44:33 +0100

    Incremented version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='SafeAPI',
 
-    version='0.0.2',
+    version='0.0.3',
 
     description='A python wrapper around the Safe Launcher API.',
     long_description=long_description,
