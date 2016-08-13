40982341bea550e7817eb4b018f70f141b875a94
William Hutcheson <hintofbasil@github.com>
2016-08-13 11:08:01 +0100

    Updated upload_webpage example

--- examples/upload_webpage.py
+++ examples/upload_webpage.py
@@ -9,30 +9,27 @@ filename = '/index.html'
 fileData = '<html><body><h1>Test successful</h1></body></html>'
 dnsName = 'DNSNAME'
 
+# Allow easy overriding of constants for testing
 try:
     dnsName = sys.argv[1]
     fileData = sys.argv[2]
 except IndexError:
-    pass # Allow easy overriding of constants for testing
+    pass
 
 if __name__=='__main__':
     # Create Safe opject
     s = Safe('Website Upload Test', '0.0.1', 'hintofbasil',
-            'com.github.hintofbasil', isShared=True)
+            'com.github.hintofbasil')
     # Authenticate against safe network
     if s.authenticate(permissions=['SAFE_DRIVE_ACCESS']):
         # Check if folder already exists
-        if not s.get_dir(folder):
+        if not s.get_dir('drive', folder):
             # If it doesn't exist create folder
-            s.mkdir(folder, False, False)
-        exit()
+            s.mkdir('drive', folder)
         #Check if file exists
-        if not s.get_file(folder + filename, True):
+        if not s.read_file('drive', folder + filename):
             # If file doesn't exist create file
-            s.post_file(folder + filename, True, False, True)
-        exit()
-        # Put data in file - overwrites current content
-        s.put_file(fileData, folder + filename)
+            s.create_file('drive', folder + filename, fileData)
         # TODO get_dns
         # Create DNS entry
-        s.post_dns(dnsName, 'www', folder)
+        s.register_dns('drive', dnsName, 'www', folder)
