5724cc27492e13d93e328072a9cd493954a8632e
William Hutcheson <hintofbasil@github.com>
2016-08-14 12:33:30 +0100

    Updated version to 0.0.4

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='SafeAPI',
 
-    version='0.0.3',
+    version='0.0.4',
 
     description='A python wrapper around the Safe Launcher API.',
     long_description=long_description,
