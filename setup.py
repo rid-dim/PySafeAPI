98e474359f8da2588a4fd4b8ebb177a4c13a85a0
hintofbasil <hintofbasil@github.com>
2016-08-19 17:07:48 +0100

    Updated to version 0.0.5 and exlcuded tests from PyPi

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='SafeAPI',
 
-    version='0.0.4',
+    version='0.0.5',
 
     description='A python wrapper around the Safe Launcher API.',
     long_description=long_description,
@@ -54,7 +54,7 @@ setup(
 
     keywords='Maidsafe Safe API',
 
-    packages=find_packages(exclude=['examples']),
+    packages=find_packages(exclude=['examples', 'tests']),
 
     install_requires=['requests']
 )
